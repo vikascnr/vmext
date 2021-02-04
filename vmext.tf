0e6f7fd0582dd3b025a2af36f7fe7ec194bfc6bf
vikascnr <vikassinghcnr@gmail.com>
2021-02-04 17:42:22 +0530

    Update vmext.tf

--- vmext.tf
+++ vmext.tf
@@ -56,17 +56,3 @@ resource "azurerm_windows_virtual_machine" "example" {
 ##########################################################
 ## Install IIS on VM
 ##########################################################
-
-resource "azurerm_virtual_machine_extension" "iis" {
-  name                 = "install-iis"
-  virtual_machine_id = azurerm_windows_virtual_machine.example.id
-  publisher            = "Microsoft.Compute"
-  type                 = "CustomScriptExtension"
-  type_handler_version = "1.9"
-
-  settings = <<SETTINGS
-    { 
-      "commandToExecute": "powershell Add-WindowsFeature Web-Asp-Net45;Add-WindowsFeature NET-Framework-45-Core;Add-WindowsFeature Web-Net-Ext45;Add-WindowsFeature Web-ISAPI-Ext;Add-WindowsFeature Web-ISAPI-Filter;Add-WindowsFeature Web-Mgmt-Console;Add-WindowsFeature Web-Scripting-Tools;Add-WindowsFeature Search-Service;Add-WindowsFeature Web-Filtering;Add-WindowsFeature Web-Basic-Auth;Add-WindowsFeature Web-Windows-Auth;Add-WindowsFeature Web-Default-Doc;Add-WindowsFeature Web-Http-Errors;Add-WindowsFeature Web-Static-Content;"
-    } 
-SETTINGS
-}
